b679b6f7efe1243f4ab89a8bf043dd9c8152c83d
adarshnikam17 <adarshnikam256@gmail.com>
2026-03-13 21:57:10 +0530

    Fix navbar ESLint error + add new projects

--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -175,9 +175,10 @@ const Navbar = () => {
    <Nav>
     <NavbarContainer>
         <NavLogo to="/">
-        <a style={{ display: "flex", alignItems: "center", color: "white", marginBottom: '20;', cursor: 'pointer' }}>
+          {/* FIX: replaced <a> with <div> to fix ESLint anchor-is-valid error */}
+          <div style={{ display: "flex", alignItems: "center", color: "white", marginBottom: '20', cursor: 'pointer' }}>
             <DiCssdeck size="3rem" /> <Span>Adarsh Nikam</Span>
-          </a>
+          </div>
         </NavLogo>
         <MobileIcon>
         <FaBars onClick={() => {
@@ -192,7 +193,7 @@ const Navbar = () => {
           <NavLink href='#education'>Education</NavLink>
         </NavItems>
         <ButtonContainer>
-            <GitHubButton href={Bio.github}>
+            <GitHubButton href={Bio.github} target="_blank" rel="noopener noreferrer">
                 Github profile
             </GitHubButton>
         </ButtonContainer>
@@ -214,7 +215,7 @@ const Navbar = () => {
               <MobileLink href='#education' onClick={() => {
                 setIsOpen(!isOpen)
               }}>Education</MobileLink>
-              <GitHubButton style={{padding: '10px 16px',background: `${theme.primary}`, color: 'white',width: 'max-content'}} href="/" target="_blank">Github Profile</GitHubButton>
+              <GitHubButton style={{padding: '10px 16px',background: `${theme.primary}`, color: 'white',width: 'max-content'}} href={Bio.github} target="_blank" rel="noopener noreferrer">Github Profile</GitHubButton>
             </MobileMenu>
         }
     </NavbarContainer>
